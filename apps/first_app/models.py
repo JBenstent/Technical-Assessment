6f7b9fb7576ef137576c927ac3fbacaddf9599d1
Jonathan Benstent <jonbenstent@gmail.com>
2017-07-20 11:48:13 -0500

    Addded JavaScript functionality for Question 1

--- apps/first_app/models.py
+++ apps/first_app/models.py
@@ -1,8 +1,2 @@
 from __future__ import unicode_literals
 from django.db import models
-
-
-class Automobiles(models.Model):
-    Founder = models.CharField(max_length=255)
-    Manufacturer = models.CharField(max_length=255)
-    Model = models.CharField(max_length=50)
--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import Automobiles
 
 def homepage(request):
 
